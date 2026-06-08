279f5ace243579ab3a27a3c4098de04750b114c5
ghmeundong <89688238+ghmeundong@users.noreply.github.com>
2026-06-08 16:50:44 +0900

    Update notification.js

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -3,7 +3,7 @@ const PushSubscription = require("../models/pushSubscription");
 
 const VAPID_PUBLIC_KEY = process.env.VAPID_PUBLIC_KEY || "";
 const VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY || "";
-const VAPID_EMAIL = process.env.VAPID_EMAIL || "mailto:admin@example.com";
+const VAPID_EMAIL = process.env.VAPID_EMAIL || "";
 
 if (VAPID_PUBLIC_KEY && VAPID_PRIVATE_KEY) {
   webpush.setVapidDetails(VAPID_EMAIL, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY);
